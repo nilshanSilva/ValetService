13e916842c17f60673b9b271dc9576e11787967a
Nilshan Silva <nilshandevinda1@gmail.com>
2017-12-01 10:44:25 +0530

    Add project files.

---
+++ ValetService/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(ValetService.Startup))]
+
+namespace ValetService
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureMobileApp(app);
+        }
+    }
+}
